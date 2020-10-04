2d579c6ee75cf45d69082ea75bd4a57aa3bdb267
Nero <nerochen25@berkeley.edu>
2020-10-04 16:32:47 -0700

    connect redux to react for all reports

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import reportReducer from './reportReducer';
+
+const rootReducer = combineReducers({
+    reports: reportReducer
+})
+
+export default rootReducer;
