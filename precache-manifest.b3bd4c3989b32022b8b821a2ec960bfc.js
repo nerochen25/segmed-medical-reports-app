c430b696e8937e15dd4722cfe393998e14dcb51f
Nero <nerochen25@berkeley.edu>
2020-10-04 22:45:51 -0700

    Updates

---
+++ precache-manifest.b3bd4c3989b32022b8b821a2ec960bfc.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6d8fcd0537ca751735bec8b7615e65",
+    "url": "/segmed-medical-reports-app/index.html"
+  },
+  {
+    "revision": "9268749170741abd72b4",
+    "url": "/segmed-medical-reports-app/static/css/main.38b45e96.chunk.css"
+  },
+  {
+    "revision": "ad679d302ddf0ffbbcc4",
+    "url": "/segmed-medical-reports-app/static/js/2.1da099a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/segmed-medical-reports-app/static/js/2.1da099a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9268749170741abd72b4",
+    "url": "/segmed-medical-reports-app/static/js/main.02796f0d.chunk.js"
+  },
+  {
+    "revision": "fcb0a6ba73563521fda5",
+    "url": "/segmed-medical-reports-app/static/js/runtime-main.1bf865e5.js"
+  },
+  {
+    "revision": "84642aa9f2d9951778d443dd22b54b5d",
+    "url": "/segmed-medical-reports-app/static/media/report1.84642aa9.txt"
+  },
+  {
+    "revision": "9a69c1034e517eca9c21f46d9d55984d",
+    "url": "/segmed-medical-reports-app/static/media/report10.9a69c103.txt"
+  },
+  {
+    "revision": "71095f86e56494d31712d2840fc563ab",
+    "url": "/segmed-medical-reports-app/static/media/report11.71095f86.txt"
+  },
+  {
+    "revision": "4d35b64c3607a788a3397181b26142bb",
+    "url": "/segmed-medical-reports-app/static/media/report12.4d35b64c.txt"
+  },
+  {
+    "revision": "2a66aa367e6c4a986f80f136954e4878",
+    "url": "/segmed-medical-reports-app/static/media/report13.2a66aa36.txt"
+  },
+  {
+    "revision": "d405efffd10b9d1de050457f456a1c23",
+    "url": "/segmed-medical-reports-app/static/media/report14.d405efff.txt"
+  },
+  {
+    "revision": "4bfbdcac0f6103eb6667af50f59a05ae",
+    "url": "/segmed-medical-reports-app/static/media/report15.4bfbdcac.txt"
+  },
+  {
+    "revision": "92063f2168fb82c83062cf10984b3c79",
+    "url": "/segmed-medical-reports-app/static/media/report16.92063f21.txt"
+  },
+  {
+    "revision": "f407d0b0ba53074961b459bc24161d92",
+    "url": "/segmed-medical-reports-app/static/media/report17.f407d0b0.txt"
+  },
+  {
+    "revision": "827bdcbf609f5ab5e4f48995c1247ac3",
+    "url": "/segmed-medical-reports-app/static/media/report18.827bdcbf.txt"
+  },
+  {
+    "revision": "28a88ddaf006ae105ce4d4243cc17561",
+    "url": "/segmed-medical-reports-app/static/media/report19.28a88dda.txt"
+  },
+  {
+    "revision": "9f922a3850c04c44ada212235d4c4228",
+    "url": "/segmed-medical-reports-app/static/media/report2.9f922a38.txt"
+  },
+  {
+    "revision": "e574e7aab35e63f783628b5953d7e6c6",
+    "url": "/segmed-medical-reports-app/static/media/report20.e574e7aa.txt"
+  },
+  {
+    "revision": "b5b4974ccb32747a86f115955827aff5",
+    "url": "/segmed-medical-reports-app/static/media/report21.b5b4974c.txt"
+  },
+  {
+    "revision": "d8ea3f22eb80834d9444e4febb99afff",
+    "url": "/segmed-medical-reports-app/static/media/report22.d8ea3f22.txt"
+  },
+  {
+    "revision": "b0aa132491fef6da8bc0ca7f1ea2830c",
+    "url": "/segmed-medical-reports-app/static/media/report23.b0aa1324.txt"
+  },
+  {
+    "revision": "6ede5bded6d0e81291e1194740b65f5b",
+    "url": "/segmed-medical-reports-app/static/media/report24.6ede5bde.txt"
+  },
+  {
+    "revision": "42946e45daa2607625f5aedc6ea3f8a7",
+    "url": "/segmed-medical-reports-app/static/media/report25.42946e45.txt"
+  },
+  {
+    "revision": "f458a9bb7c498ff8d3acd446374396a7",
+    "url": "/segmed-medical-reports-app/static/media/report26.f458a9bb.txt"
+  },
+  {
+    "revision": "1490bb14184f83bb09172e9b5d22279e",
+    "url": "/segmed-medical-reports-app/static/media/report27.1490bb14.txt"
+  },
+  {
+    "revision": "3ed2785ef45233af406e649129dd054f",
+    "url": "/segmed-medical-reports-app/static/media/report28.3ed2785e.txt"
+  },
+  {
+    "revision": "7e6e4335944ac448f28b2a648c922b45",
+    "url": "/segmed-medical-reports-app/static/media/report29.7e6e4335.txt"
+  },
+  {
+    "revision": "68f0623f90759095bed09be032910194",
+    "url": "/segmed-medical-reports-app/static/media/report3.68f0623f.txt"
+  },
+  {
+    "revision": "51fb7c42d8d9b5f249cd35e04b66f320",
+    "url": "/segmed-medical-reports-app/static/media/report30.51fb7c42.txt"
+  },
+  {
+    "revision": "3439e63768399b3171dcfcf6e016a9c9",
+    "url": "/segmed-medical-reports-app/static/media/report31.3439e637.txt"
+  },
+  {
+    "revision": "fb7869e2e8737b78b706670effd8e79f",
+    "url": "/segmed-medical-reports-app/static/media/report32.fb7869e2.txt"
+  },
+  {
+    "revision": "acc2aae4d9651e32e8e3469700ca030f",
+    "url": "/segmed-medical-reports-app/static/media/report33.acc2aae4.txt"
+  },
+  {
+    "revision": "d697613effeb21d45e5819946e8d7b7b",
+    "url": "/segmed-medical-reports-app/static/media/report34.d697613e.txt"
+  },
+  {
+    "revision": "5210c6c9c84bacd7d05c4634c9308f7c",
+    "url": "/segmed-medical-reports-app/static/media/report35.5210c6c9.txt"
+  },
+  {
+    "revision": "3a382e8e3cd5c2593f5e646b1c9be4a4",
+    "url": "/segmed-medical-reports-app/static/media/report36.3a382e8e.txt"
+  },
+  {
+    "revision": "66b2f5310a75364d6d6e9d9fbca5a30f",
+    "url": "/segmed-medical-reports-app/static/media/report37.66b2f531.txt"
+  },
+  {
+    "revision": "6c764a7a9334252241788887d1c273e5",
+    "url": "/segmed-medical-reports-app/static/media/report38.6c764a7a.txt"
+  },
+  {
+    "revision": "4bd631b828a4bf6ff6ff3dc1c14f5124",
+    "url": "/segmed-medical-reports-app/static/media/report39.4bd631b8.txt"
+  },
+  {
+    "revision": "f0d65e6205b3f879d0a43fe4be452cfe",
+    "url": "/segmed-medical-reports-app/static/media/report4.f0d65e62.txt"
+  },
+  {
+    "revision": "129bddf69978045eb2761b1f6c80fcd2",
+    "url": "/segmed-medical-reports-app/static/media/report40.129bddf6.txt"
+  },
+  {
+    "revision": "e983ccc8e43242e12b0ca51ed65af235",
+    "url": "/segmed-medical-reports-app/static/media/report41.e983ccc8.txt"
+  },
+  {
+    "revision": "804cd0f7458d27b565ac180d4013b194",
+    "url": "/segmed-medical-reports-app/static/media/report42.804cd0f7.txt"
+  },
+  {
+    "revision": "00d9d449a041cc3641acc8b9ea3ddab6",
+    "url": "/segmed-medical-reports-app/static/media/report43.00d9d449.txt"
+  },
+  {
+    "revision": "88a51a2c7a06e1e568481e51f84dd7ef",
+    "url": "/segmed-medical-reports-app/static/media/report44.88a51a2c.txt"
+  },
+  {
+    "revision": "f68b7d3a77214691696443dd2066b760",
+    "url": "/segmed-medical-reports-app/static/media/report45.f68b7d3a.txt"
+  },
+  {
+    "revision": "578cdf266b2f661aebb9f2f915271668",
+    "url": "/segmed-medical-reports-app/static/media/report46.578cdf26.txt"
+  },
+  {
+    "revision": "3a84b2088cabbdd40e06b2889fe7c22a",
+    "url": "/segmed-medical-reports-app/static/media/report47.3a84b208.txt"
+  },
+  {
+    "revision": "358aa86225968e6d7731c2864226ed6c",
+    "url": "/segmed-medical-reports-app/static/media/report48.358aa862.txt"
+  },
+  {
+    "revision": "538f8034fa33aa6fe7b1d9e1e818ce35",
+    "url": "/segmed-medical-reports-app/static/media/report49.538f8034.txt"
+  },
+  {
+    "revision": "050df78521ee6a2fd8badfd311706c1c",
+    "url": "/segmed-medical-reports-app/static/media/report5.050df785.txt"
+  },
+  {
+    "revision": "3d10fba5f1cc3787cff6faaaaf5c1953",
+    "url": "/segmed-medical-reports-app/static/media/report50.3d10fba5.txt"
+  },
+  {
+    "revision": "adc1df89f4b29c10a09ea657b4cea4d8",
+    "url": "/segmed-medical-reports-app/static/media/report51.adc1df89.txt"
+  },
+  {
+    "revision": "b142b53c9e9711ba84c5e38ba1919f09",
+    "url": "/segmed-medical-reports-app/static/media/report6.b142b53c.txt"
+  },
+  {
+    "revision": "3edb3d419d44665c57cc1bb474967443",
+    "url": "/segmed-medical-reports-app/static/media/report7.3edb3d41.txt"
+  },
+  {
+    "revision": "af4717048930e9183861298abd75038c",
+    "url": "/segmed-medical-reports-app/static/media/report8.af471704.txt"
+  },
+  {
+    "revision": "73d1b175a0806441ccd9188e8a077729",
+    "url": "/segmed-medical-reports-app/static/media/report9.73d1b175.txt"
+  }
+]);
